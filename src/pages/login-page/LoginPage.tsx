22dfe995f639b219f919c88ed231f618e586caae
Teerasej <teerasej@gmail.com>
2021-03-18 12:09:51 +0700

    กำหนด validation schema ด้วย yup สำหรับ email และ password field

--- src/pages/login-page/LoginPage.tsx
+++ src/pages/login-page/LoginPage.tsx
@@ -6,6 +6,18 @@ import * as yup from 'yup';
 
 export default function LoginPage() {
 
+
+
+    const validationSchema = yup.object({
+        email: yup
+            .string()
+            .email('Enter a valid email')
+            .required('Email is required'),
+        password: yup
+            .string()
+            .min(8, 'Password should be of minimum 8 characters length')
+            .required('Password is required'),
+    });
     const history = useHistory();
 
     const goRegisterPage = () => {
