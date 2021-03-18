6a178a87c1457e2987ba63f7e803dd79b3a161dd
Teerasej <teerasej@gmail.com>
2021-03-18 14:33:45 +0700

    นำ formik กับ yup มาใช้ในหน้าลงทะเบียน

--- src/pages/register-page/RegisterPage.tsx
+++ src/pages/register-page/RegisterPage.tsx
@@ -1,14 +1,44 @@
 import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField } from '@material-ui/core'
 import React, { useState } from 'react'
 import { useHistory } from 'react-router-dom';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
 
 export default function RegisterPage() {
+
+    const validationSchema = yup.object({
+        email: yup
+            .string()
+            .email('Enter a valid email')
+            .required('Email is required'),
+        password: yup
+            .string()
+            .min(8, 'Password should be of minimum 8 characters length')
+            .required('Password is required'),
+    });
+
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+        },
+        validationSchema: validationSchema,
+        onSubmit: (values) => {
+            
+            const jsonFormData = JSON.stringify(values, null, 2);
+            console.log('Registration Form:', jsonFormData);
+            onStartRegisteration(jsonFormData);
+        },
+    });
 
     const history = useHistory();
 
     const [onRegisterSuccesful, setOnRegisterSuccesful] = useState(false);
 
-    const onStartRegisteration = () => {
+    const onStartRegisteration = (jsonFormData:any) => {
+
+        // register new account
+
         setOnRegisterSuccesful(true);
     }
 
@@ -21,15 +51,36 @@ export default function RegisterPage() {
 
         <div>
             <h1>Create new account</h1>
-            <p>Email:</p>
-            <TextField />
-            <p>Password:</p>
-            <TextField />
-            <div>
-                <Button variant="contained" onClick={onStartRegisteration}>
-                    Register
-                </Button>
-            </div>
+            <form onSubmit={formik.handleSubmit}>
+                <div>
+                    <TextField
+                        id="email"
+                        name="email"
+                        label="Email"
+                        value={formik.values.email}
+                        onChange={formik.handleChange}
+                        error={formik.touched.email && Boolean(formik.errors.email)}
+                        helperText={formik.touched.email && formik.errors.email}
+                    />
+                </div>
+                <div>
+                    <TextField
+                        id="password"
+                        name="password"
+                        label="Password"
+                        type="password"
+                        value={formik.values.password}
+                        onChange={formik.handleChange}
+                        error={formik.touched.password && Boolean(formik.errors.password)}
+                        helperText={formik.touched.password && formik.errors.password}
+                    />
+                </div>
+                <div>
+                    <Button variant="contained" type="submit">
+                        Register
+                    </Button>
+                </div>
+            </form>
             <Dialog
                 open={onRegisterSuccesful}
                 aria-labelledby="alert-dialog-title"
@@ -44,7 +95,7 @@ export default function RegisterPage() {
                 <DialogActions>
                     <Button onClick={onConfirmRegistration} color="primary">
                         ok
-          </Button>
+                    </Button>
                 </DialogActions>
             </Dialog>
         </div>
