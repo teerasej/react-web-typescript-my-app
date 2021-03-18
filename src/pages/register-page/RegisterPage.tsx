57f153765d1286cb649df1d81f5a3e7e752f9444
Teerasej <teerasej@gmail.com>
2021-03-18 11:43:16 +0700

    แสดง dialog แจ้งให้ login ถ้าการลงทะเบียนสำเร็จ

--- src/pages/register-page/RegisterPage.tsx
+++ src/pages/register-page/RegisterPage.tsx
@@ -1,7 +1,16 @@
-import { Button, TextField } from '@material-ui/core'
-import React from 'react'
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField } from '@material-ui/core'
+import React, { useState } from 'react'
 
 export default function RegisterPage() {
+
+    const [onRegisterSuccesful, setOnRegisterSuccesful] = useState(false);
+
+    const onStartRegisteration = () => {
+        setOnRegisterSuccesful(true);
+    }
+
+
+
     return (
 
         <div>
@@ -11,10 +20,27 @@ export default function RegisterPage() {
             <p>Password:</p>
             <TextField />
             <div>
-                <Button variant="contained">
+                <Button variant="contained" onClick={onStartRegisteration}>
                     Register
                 </Button>
             </div>
+            <Dialog
+                open={onRegisterSuccesful}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">{"Use Google's location service?"}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        Registration Completed! Please login with your account.
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button  color="primary">
+                        ok
+          </Button>
+                </DialogActions>
+            </Dialog>
         </div>
     )
 }
